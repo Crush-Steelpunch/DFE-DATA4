4ed38c5b3e422e1abaa41fe5ea93662c12f8d59c
Leon L. Robinson <leon.robinson@qa.com>
2021-11-15 11:59:32 +0000

    methods

--- intro_vars.py
+++ intro_vars.py
@@ -1,3 +1,15 @@
-numone = int(input("please enter a number"))
-numtwo = input("Please enter a number")
-print( str(numone) + numtwo )
+# numone = int(input("please enter a number"))
+# numtwo = input("Please enter a number")
+# print( str(numone) + numtwo )
+
+# number1 = float(input("Please enter the first number: "))
+# number2 = float(input("Please enter the second number: "))
+# answer = number1 + number2
+
+#print(type(numtwo))
+
+textvar1 = "I like pies"
+numvar1 = 42
+
+textvar2 = textvar1.count("i")
+print(textvar2)

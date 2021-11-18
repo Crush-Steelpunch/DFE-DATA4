bb65d402b152f7a9780b77f0b475f1dcb3ae13c7
Leon L. Robinson <leon.robinson@qa.com>
2021-11-18 11:38:03 +0000

    more budgeting

--- lr_budget.py
+++ lr_budget.py
@@ -1,20 +1,35 @@
 class Budget:
 
-    balance = 0
+    def __init__(self):
+        self.balance = 0.0
 
     def deposit(self,moneyin):
-        pass
+        self.balance += moneyin
 
     def withdraw(self,moneyout):
-        pass
+        self.balance -= moneyout
+    
+    def transferto(self,account,value):
+        self.withdraw(value)
+        account.deposit(value)
+    
+
 
 
 food = Budget()
 clothes = Budget()
 house = Budget()
 
-food.deposit(5)
+food.deposit(100)
 food.withdraw(1.99)
-
-
-
+print("food", food.balance)
+food.transferto(clothes, 30)
+print("food:", food.balance)
+print("clothes:",clothes.balance)
+clothes.transferto(house, 15)
+print("clothes:",clothes.balance)
+print("house:",house.balance)
+total = 0
+for i in [food,clothes,house]:
+    total += i.balance
+print(total)

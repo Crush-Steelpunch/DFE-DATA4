1634c71bd621b4f08f32ece227fbe01d8a0f651a
Leon L. Robinson <leon.robinson@qa.com>
2021-11-18 11:03:02 +0000

    The Budget

---
+++ lr_budget.py
@@ -0,0 +1,20 @@
+class Budget:
+
+    balance = 0
+
+    def deposit(self,moneyin):
+        pass
+
+    def withdraw(self,moneyout):
+        pass
+
+
+food = Budget()
+clothes = Budget()
+house = Budget()
+
+food.deposit(5)
+food.withdraw(1.99)
+
+
+

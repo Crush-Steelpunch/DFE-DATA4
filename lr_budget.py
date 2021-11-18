04b3af929b16077bf0e90f6712cd0d6d3a0dd461
Leon L. Robinson <leon.robinson@qa.com>
2021-11-18 13:37:40 +0000

    budget with reasoning

--- lr_budget.py
+++ lr_budget.py
@@ -1,3 +1,30 @@
+# Goal: “Create a Budget class that can instantiate objects based on different budget categories
+# - Create Class what will make a few objects
+# - cat1 = Class()
+# - cat2 = Class()
+# like food, clothing, and entertainment. 
+
+
+# These objects should allow for depositing and withdrawing funds from each category, 
+# - Action, Therefor I need to write methods
+#   def depost()  withdr()
+
+# as well computing category balances and 
+# - Classes are not just code, they are also data! Therefore a balance variable.
+# - all transactions must use the balance var
+
+# transferring balance amounts between categories”
+# - what info do I need? 
+#   - from cat name - this is the object we created
+#   - to cat name  - this is the deposit method in another class object
+#   - balance amount
+
+
+import pdb
+pdb.set_trace()
+
+
+
 class Budget:
 
     def __init__(self):
@@ -12,13 +39,17 @@ class Budget:
     def transferto(self,account,value):
         self.withdraw(value)
         account.deposit(value)
-    
 
 
 
 food = Budget()
 clothes = Budget()
 house = Budget()
+entertainment = Budget()
+pets = Budget()
+
+pets.deposit(120)
+
 
 food.deposit(100)
 food.withdraw(1.99)
